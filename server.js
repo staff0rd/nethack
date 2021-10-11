1d1cebcec56e4dfec0a594a95b31e853dc433d73
Stafford Williams <stafford.williams@gmail.com>
2021-10-11 21:24:11 +1100

    ssh approach

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
+app.set("view engine", "ejs");
+app.use(
+  express.urlencoded({
+    extended: false,
+    limit: "150mb",
+  })
+);
+app.use(express.static(__dirname + "/public"));
+app.use("/xterm.css", express.static(require.resolve("xterm/css/xterm.css")));
+app.use("/xterm.js", express.static(require.resolve("xterm")));
+app.use(
+  "/xterm-addon-fit.js",
+  express.static(require.resolve("xterm-addon-fit"))
+);
+
+const SSHClient = require("ssh2").Client;
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+io.on("connection", function (socket) {
+  var conn = new SSHClient();
+  conn
+    .on("ready", function () {
+      socket.emit("data", "\r\n*** SSH CONNECTION ESTABLISHED ***\r\n");
+      conn.shell(function (err, stream) {
+        if (err)
+          return socket.emit(
+            "data",
+            "\r\n*** SSH SHELL ERROR: " + err.message + " ***\r\n"
+          );
+        socket.on("data", function (data) {
+          stream.write(data);
+        });
+        stream
+          .on("data", function (d) {
+            socket.emit("data", d.toString("binary"));
+          })
+          .on("close", function () {
+            conn.end();
+          });
+      });
+    })
+    .on("close", function () {
+      socket.emit("data", "\r\n*** SSH CONNECTION CLOSED ***\r\n");
+    })
+    .on("error", function (err) {
+      socket.emit(
+        "data",
+        "\r\n*** SSH CONNECTION ERROR: " + err.message + " ***\r\n"
+      );
+    })
+    .connect({
+      host: "alt.org",
+      port: 22,
+      username: "nethack",
+      password: "",
+    });
+});
+
+http.listen(3000, () => {
+  console.log("Listening on http://localhost:3000");
+});
