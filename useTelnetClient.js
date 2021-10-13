08304e8add63bb200168257c3ea75c573fcc799c
Stafford Williams <stafford.williams@gmail.com>
2021-10-13 21:29:11 +1100

    write

--- useTelnetClient.js
+++ useTelnetClient.js
@@ -44,7 +44,7 @@ const useTelnetClient = (io) => {
               "\r\n*** TELNET SHELL ERROR: " + err.message + " ***\r\n"
             );
           socket.on("data", function (data) {
-            [...data].forEach((c) => stream.write(data));
+            stream.write(data);
           });
           stream
             .on("data", function (d) {
